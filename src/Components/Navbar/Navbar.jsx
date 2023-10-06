6ab26cbb921726b57e0312cd73b209c4b5a49b64
Masum-Reza <careerhunter4280@gmail.com>
2023-10-06 02:32:05 -0700

    Navbar routing with navlink completed

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,18 +1,23 @@
-import { Link } from 'react-router-dom'
+import './Navbar.css'
 import logo from '../../assets/images/logo.png'
+import { NavLink } from 'react-router-dom'
+import { BsSearch } from 'react-icons/bs';
 
 const Navbar = () => {
     return (
-        <div className='flex items-center justify-center gap-5 pt-5'>
+        <div className='flex flex-col md:flex-row items-center justify-center gap-5 pt-5'>
             <img className='w-[100px] invert' src={logo} alt="" />
-            <input className='border bg-transparent rounded-md px-3 focus:outline-none' placeholder='Search your destination' type="text" />
-            <ul className='flex gap-5'>
-                <li><Link to={'/news'}>News</Link></li>
-                <li><Link to={'/destination'}>Destination</Link></li>
-                <li><Link to={'/blog'}>Blog</Link></li>
-                <li><Link to={'/contact'}>Contact</Link></li>
-                <li><Link to={'/login'}>Login</Link></li>
-                <li><Link to={'/register'}>Registration</Link></li>
+            <div className='flex items-center relative'>
+                <input className='border bg-transparent rounded-md px-9 py-1 focus:outline-none' placeholder='Search your destination' type="text" />
+                <BsSearch className='absolute text-xl mx-3' />
+            </div>
+            <ul className='flex flex-col md:flex-row gap-5'>
+                <li><NavLink to={'/news'}>News</NavLink></li>
+                <li><NavLink to={'/destination'}>Destination</NavLink></li>
+                <li><NavLink to={'/blog'}>Blog</NavLink></li>
+                <li><NavLink to={'/contact'}>Contact</NavLink></li>
+                <li><NavLink to={'/login'}>Login</NavLink></li>
+                <li><NavLink to={'/register'}>Registration</NavLink></li>
             </ul>
         </div>
     )
